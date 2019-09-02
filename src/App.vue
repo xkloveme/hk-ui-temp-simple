f1ba92e509af9bb74f0b0c3e4f67e17fea441571
superDragon <admin@jixiaokang.com>
2019-09-02 14:51:51 +0800

    feat:simple temp demo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
+<!--
+ * @describe: 描述
+ * @Author: superDragon
+ * @Date: 2019-06-26 09:50:23
+ * @LastEditors: superDragon
+ * @LastEditTime: 2019-09-02 14:49:32
+ -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      <a
+        href="https://hy.hekr.me/hk-ui/web/component/hk-button.html"
+        style="text-decoration: blink;"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img class="img" :style="{ backgroundColor: this.color }" src="./assets/logo.png" alt="hekr logo" />
+        欢迎使用HEKR-UI
+      </a>
+    </h1>
+    <hk-button type="primary">Element-ui拓展的组件库</hk-button>
+    <hk-button type="info">通过少量的配置来生成页面</hk-button>
+    <hk-button type="warning">让开发更简单</hk-button>
+    <hk-button type="danger">Easy Code</hk-button>
+    <h2>您已成功配置！！！</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      value: '44'
+    }
+  },
+  computed: {
+    color () {
+      return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    }
   }
 }
 </script>
@@ -24,5 +50,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.img {
+  width: 300px;
+  height: 100px;
+  background-color: antiquewhite;
+  display: block;
+  text-align: center;
+  margin: 0 auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
+/*
+ * @describe: 描述
+ * @Author: superDragon
+ * @Date: 2019-06-26 09:50:23
+ * @LastEditors: superDragon
+ * @LastEditTime: 2019-09-02 14:03:39
+ */
 import Vue from 'vue'
 import App from './App.vue'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import HkUI from 'hekr-ui'
+import 'hekr-ui/lib/hk-ui.css';
+// Vue.config.productionTip = false
 
-Vue.config.productionTip = false
-
+Vue.use(Element)
+Vue.use(HkUI)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
